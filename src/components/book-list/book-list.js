052defea638c4fc820ed7c93d6ec97649a02941d
Yaroslav Magrelo <slava.magrelo@gmail.com>
2021-07-29 22:52:03 +0300

    add async data and spinner

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -5,15 +5,21 @@ import BookListItem from '../book-list-item/book-list-item';
 import { connect } from 'react-redux';
 import withBookstoreService from '../hoc/with-bookstore-service';
 import { booksLoaded } from '../../actions/index';
+import Spinner from '../spinner/spinner';
 
 class BookList extends Component {
   componentDidMount() {
-    const { bookstoreService } = this.props;
-    const data = bookstoreService.getBooks();
-    this.props.booksLoaded(data);
+    const { bookstoreService, booksLoaded } = this.props;
+    bookstoreService.getBooks().then((data) => {
+      booksLoaded(data);
+    });
   }
   render() {
-    const { books } = this.props;
+    const { books, loading } = this.props;
+
+    if (loading) {
+      return <Spinner />;
+    }
 
     return (
       <ul className="book-list">
@@ -25,9 +31,10 @@ class BookList extends Component {
   }
 }
 
-const mapStateToProps = ({ books }) => {
+const mapStateToProps = ({ books, loading }) => {
   return {
-    books: books,
+    books,
+    loading,
   };
 };
 
